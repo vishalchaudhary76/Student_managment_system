c0278cd0003289e0ac44417eb5d60aca106707c1
vishalchaudhary76 <vishaltanger92@gmail.com>
2025-10-25 13:30:44 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+class Config:
+    # MySQL Configuration
+    MYSQL_HOST = "localhost"
+    MYSQL_USER = "student_user"  # or "root" if using root
+    MYSQL_PASSWORD = "student123"  # use the password you set
+    MYSQL_DATABASE = "student_management"
+    MYSQL_PORT = 3306
